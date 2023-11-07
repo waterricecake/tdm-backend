0e35ad789c7ee4f93736d948522c44f056049b2f
waterricecake <fruturum@nate.com>
2023-11-07 16:53:53 +0900

    chore: init

---
+++ src/main/java/tdm/tdmbackend/TdmBackendApplication.java
@@ -0,0 +1,13 @@
+package tdm.tdmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdm/tdmbackend/TdmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tdm.tdmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
