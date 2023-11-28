1ed4f872baec7c375c483e867b309fbe4f8fa9cf
waterricecake <fruturum@nate.com>
2023-11-28 18:19:46 +0900

    test: 초기 데이터 sql 스크립트 작성

---
+++ src/test/resources/testdata/member.sql
@@ -0,0 +1,2 @@
+INSERT INTO member(nickname, profile, social_id, school,created_at,updated_at,status)
+values ('TestMember','https://TestClass.jpg','12331323123','test고',now(),now(),'USABLE');
---
+++ src/test/resources/testdata/post.sql
@@ -0,0 +1,6 @@
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test1','content',now(),now(),'USABLE');
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test2','content',now(),now(),'USABLE');
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test3','content',now(),now(),'USABLE');
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test4','content',now(),now(),'USABLE');
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test5','content',now(),now(),'USABLE');
+INSERT INTO post(member_id,title, content,created_at,updated_at,status) values (1,'test6','content',now(),now(),'USABLE');
