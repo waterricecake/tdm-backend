7bd708b4b6d97e14fb9ad91c63bda0d3aea31ca6
waterricecake <fruturum@nate.com>
2023-11-28 18:22:39 +0900

    feat: 통합 및 서비스 추상 테스트 구현

--- src/test/java/tdm/tdmbackend/integration/IntegrationTest.java
+++ src/test/java/tdm/tdmbackend/integration/IntegrationTest.java
@@ -1,4 +1,4 @@
-package integration;
+package tdm.tdmbackend.integration;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -6,8 +6,21 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@Sql(
+        value = {
+                "classpath:testdata/truncate.sql",
+                "classpath:testdata/member.sql",
+                "classpath:testdata/tag.sql",
+                "classpath:testdata/post.sql",
+                "classpath:testdata/post_tag.sql",
+                "classpath:testdata/member_tag.sql",
+                "classpath:testdata/image.sql"
+
+        }
+)
 public abstract class IntegrationTest {
 
     @LocalServerPort
