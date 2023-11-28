4498dc3a7fa70ea0ae930d6a828bbc26677d8da3
waterricecake <fruturum@nate.com>
2023-11-28 19:09:35 +0900

    docs: Swagger API 문서 구축

---
+++ src/main/java/tdm/tdmbackend/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package tdm.tdmbackend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("TDM API 문서")
+                .description("TDM API 문서")
+                .version("1.0.0");
+    }
+}
