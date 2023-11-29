800acc582caec76373b14e551ca83a9be2d6dc87
waterricecake <fruturum@nate.com>
2023-11-29 16:39:48 +0900

    feat: 멤버 필드 수정

--- src/test/resources/testdata/member.sql
+++ src/test/resources/testdata/member.sql
@@ -1,2 +1,2 @@
-INSERT INTO member(nickname, profile, social_id, school, created_at, updated_at, status)
-VALUES ('TestMember', 'https://TestClass.jpg', '12331323123', 'test고', now(), now(), 'USABLE');
+INSERT INTO member(nickname, profile, social_id, school, level, created_at, updated_at, status)
+VALUES ('TestMember', 'https://TestClass.jpg', '12331323123', 'test고', 1, now(), now(), 'USABLE');
