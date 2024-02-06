403c67c217cc84bc199d5236ede2f6598463a527
waterricecake <fruturum@nate.com>
2024-02-06 18:51:57 +0900

    feat: 게시물 예외 구현

--- src/main/java/tdm/tdmbackend/global/exception/ExceptionCode.java
+++ src/main/java/tdm/tdmbackend/global/exception/ExceptionCode.java
@@ -10,7 +10,10 @@ import lombok.RequiredArgsConstructor;
 public enum ExceptionCode {
     COMMON(1000,""),
     // 2000~ 2999 (멤버관련)
-    NO_SUCH_MEMBER(2001,"해당하는 사용자가 존재하지 않습니다.")
+    NO_SUCH_MEMBER(2001,"해당하는 사용자가 존재하지 않습니다."),
+
+    // 3000~ 3999 (게시물 관련)
+    NO_SUCH_POST(3001, "해당하는 게시물이 존재하지 않습니다.")
     ;
 
     private final int errorCode;
