fe2f0d2202f1e05ab81123227eeb9c15906ae7da
waterricecake <fruturum@nate.com>
2024-02-06 18:59:05 +0900

    feat: 댓글 예외처리

--- src/main/java/tdm/tdmbackend/global/exception/ExceptionCode.java
+++ src/main/java/tdm/tdmbackend/global/exception/ExceptionCode.java
@@ -9,11 +9,14 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionCode {
     COMMON(1000,""),
-    // 2000~ 2999 (멤버관련)
+    // 2000 ~ 2999 (멤버관련)
     NO_SUCH_MEMBER(2001,"해당하는 사용자가 존재하지 않습니다."),
 
-    // 3000~ 3999 (게시물 관련)
-    NO_SUCH_POST(3001, "해당하는 게시물이 존재하지 않습니다.")
+    // 3000 ~ 3999 (게시물 관련)
+    NO_SUCH_POST(3001, "해당하는 게시물이 존재하지 않습니다."),
+
+    // 4000 ~4999 (댓글 관련)
+    NO_SUCH_COMMENT(4001,"해당하는 댓글이 존재하지 않습니다.")
     ;
 
     private final int errorCode;
--- src/test/java/tdm/tdmbackend/post/service/CommentServiceTest.java
+++ src/test/java/tdm/tdmbackend/post/service/CommentServiceTest.java
@@ -1,13 +1,19 @@
 package tdm.tdmbackend.post.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import tdm.tdmbackend.global.DtoCreater;
 import tdm.tdmbackend.global.ServiceTest;
+import tdm.tdmbackend.global.exception.BadRequestException;
 import tdm.tdmbackend.post.domain.Comment;
 import tdm.tdmbackend.post.dto.request.CommentRequest;
 import tdm.tdmbackend.post.repository.CommentRepository;
@@ -39,6 +45,31 @@ public class CommentServiceTest extends ServiceTest {
         assertThat(comment.getContent()).isEqualTo(commentRequest.getContent());
     }
 
+    @ParameterizedTest(name = "{0}일때 댓글 생성에 실패한다")
+    @MethodSource("createFailCaseProvider")
+    void create_Fail(
+            final String testCase,
+            final long postId,
+            final long memberId,
+            final Class<Exception> errorClass
+    ) {
+        // given
+        final CommentRequest commentRequest = DtoCreater.create(
+                CommentRequest.class,
+                "createContent"
+        );
+
+        assertThatThrownBy(() -> commentService.create(commentRequest, postId, memberId))
+                .isInstanceOf(errorClass);
+    }
+
+    static Stream<Arguments> createFailCaseProvider() {
+        return Stream.of(
+                Arguments.of("해당하는 게시물이 없을 때", -1L, 1L, BadRequestException.class),
+                Arguments.of("해당하는 멤버가 없을 때", 1L, -1L, BadRequestException.class)
+        );
+    }
+
     @Test
     @DisplayName("댓글을 수정한다")
     void update() {
@@ -55,6 +86,29 @@ public class CommentServiceTest extends ServiceTest {
 
         // then
         assertThat(comment.getContent()).isEqualTo(commentRequest.getContent());
+    }
+
+    @ParameterizedTest(name = "{0}일때 댓글 수정에 실패한다")
+    @MethodSource("updateFailCaseProvider")
+    void update_Fail(
+            final String testCase,
+            final long commentId,
+            final Class<Exception> errorClass
+    ) {
+        // given
+        final CommentRequest commentRequest = DtoCreater.create(
+                CommentRequest.class,
+                "createContent"
+        );
+
+        assertThatThrownBy(() -> commentService.update(commentRequest, commentId))
+                .isInstanceOf(errorClass);
+    }
+
+    static Stream<Arguments> updateFailCaseProvider() {
+        return Stream.of(
+                Arguments.of("해당하는 댓글이 없을 때", -1L, BadRequestException.class)
+        );
     }
 
     @Test
