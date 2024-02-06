103f31dd11dc2980577db1690d2ecec10e7d64e9
waterricecake <fruturum@nate.com>
2024-02-06 17:16:21 +0900

    refactor: 매직넘버 상수처리

--- src/main/java/tdm/tdmbackend/login/util/AccessTokenExtractor.java
+++ src/main/java/tdm/tdmbackend/login/util/AccessTokenExtractor.java
@@ -6,9 +6,12 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public abstract class AccessTokenExtractor {
+
+    private static final String BEARER = "Bearer ";
+
     public static String extractAccessToken(final String header) {
-        if (header != null && header.startsWith("Bearer ")) {
-            return header.substring("Bearer ".length()).trim();
+        if (header != null && header.startsWith(BEARER)) {
+            return header.substring(BEARER.length()).trim();
         }
         // todo : 예외처리
         throw new IllegalArgumentException();
